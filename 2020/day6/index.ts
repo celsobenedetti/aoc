c4a2ea2e60fc1e7e7dba7241691a9ee481493dc7
Celso Benedetti <celsobenedetti2@gmail.com>
2023-11-30 12:27:08 -0300

    2020: Day6 Part2

--- 2020/day6/index.ts
+++ 2020/day6/index.ts
@@ -1,30 +1,70 @@
-export function day6(input: string): [number, number] {
-  const groups = parseInput(input);
-  return [part1(groups), -1];
+// maps question letter to number of responses in group
+type GroupAnswers = Map<string, number>;
+
+type Group = {
+  answers: GroupAnswers;
+  size: number;
+};
+
+function newGroup(): Group {
+  return {
+    answers: new Map(),
+    size: 0,
+  };
 }
 
-function parseInput(input: string): Set<string>[] {
-  const groups: Set<string>[] = [];
+export function day6(input: string): [number, number] {
+  const groups = parseInput(input);
+  return [part1(groups), part2(groups)];
+}
 
-  let newGroup = new Set<string>();
+function parseInput(input: string): Group[] {
+  const groups: Group[] = [];
+
+  let currGroup = newGroup();
+
   for (const line of input.split("\n")) {
     if (!line.length) {
-      groups.push(newGroup);
-      newGroup = new Set();
+      groups.push(currGroup);
+      currGroup = newGroup();
       continue;
     }
 
+    currGroup.size++;
+
     for (const char of line.split("")) {
-      newGroup.add(char);
+      const count = currGroup.answers.get(char);
+      if (!count) {
+        currGroup.answers.set(char, 1);
+      } else {
+        currGroup.answers.set(char, count + 1);
+      }
     }
   }
-  groups.push(newGroup);
+  groups.push(currGroup);
 
   return groups;
 }
 
-function part1(groups: Set<string>[]): number {
+function part1(groups: Group[]): number {
   return groups.reduce((sum, group) => {
+    return sum + group.answers.size;
+  }, 0);
+}
+
+function part2(groups: Group[]): number {
+  const keepOnlyAnswersByEveryone = (group: Group): GroupAnswers => {
+    const newGroup = new Map() as GroupAnswers;
+
+    [...group.answers]
+      .filter(([_, count]) => count == group.size)
+      .forEach(([k, v]) => {
+        newGroup.set(k, v);
+      });
+    return newGroup;
+  };
+
+  return groups.map(keepOnlyAnswersByEveryone).reduce((sum, group) => {
     return sum + group.size;
   }, 0);
 }
