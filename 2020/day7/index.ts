2acfe90db341870ddf2e17d6c64f61b8a3b2251b
Celso Benedetti <celsobenedetti2@gmail.com>
2023-12-01 07:59:34 -0300

    2020(docs): Day 7 README

---
+++ 2020/day7/index.ts
@@ -0,0 +1,11 @@
+export function day7(input: string): [number, number] {
+  return [part1(), part2()];
+}
+
+function part1(): number {
+  return -1;
+}
+
+function part2(): number {
+  return -1;
+}
