24f076e06d52fb1adbf958fb0acae8b6a8398d99
Celso Benedetti <celsobenedetti2@gmail.com>
2023-12-02 14:49:46 -0300

    2023(lib): print solution runtime

--- 2023/lib/run.ts
+++ 2023/lib/run.ts
@@ -3,7 +3,10 @@ import { readFileToStr } from "./input";
 type solution = (input: string) => { day: number; p1: number; p2: number };
 
 export function run(fn: solution, file: string) {
+  const startTime = performance.now();
+
   console.log({
     ...fn(readFileToStr(file)),
+    time: (performance.now() - startTime).toFixed(3),
   });
 }
