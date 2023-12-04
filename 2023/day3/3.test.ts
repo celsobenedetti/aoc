a31aea8524a8cb713a54072fe5f7750332dfed4e
Celso Benedetti <celsobenedetti2@gmail.com>
2023-12-04 08:06:49 -0300

    2023: Day3 Part2

--- 2023/day3/3.test.ts
+++ 2023/day3/3.test.ts
@@ -1,15 +1,74 @@
 import { describe, test, expect } from "vitest";
 
 import { readFileToStr } from "../lib/input";
-import { day3 } from ".";
+import { day3, getNumberAtIndex } from ".";
+
+// 467..114..
+// ...*......
+// ..35..633.
+// ......#...
+// 617*......
+// .....+.58.
+// ..592.....
+// ......755.
+// ...$.*....
+// .664.598..
+
+const inputFile = "day3/test.input.txt";
 
 describe("Day 3", () => {
-    test("testInput", () => {
-        const input = readFileToStr("day3/test.input.txt");
+    describe("testInput", () => {
+        const input = readFileToStr(inputFile);
         const want1 = 4361;
+        const want2 = 467835;
 
-        const { p1 } = day3(input);
+        const { p1, p2 } = day3(input);
 
-        expect(p1).toBe(want1);
+        test("Part1", () => expect(p1).toBe(want1));
+        test("Part2", () => expect(p2).toBe(want2));
+    });
+
+    describe("utility functions", () => {
+        test("getNumberAtIndex", () => {
+            const lines = readFileToStr(inputFile).split("\n");
+
+            expect(getNumberAtIndex(0, lines[0])).toBe(467);
+            expect(getNumberAtIndex(1, lines[0])).toBe(467);
+            expect(getNumberAtIndex(2, lines[0])).toBe(467);
+
+            expect(getNumberAtIndex(5, lines[0])).toBe(114);
+            expect(getNumberAtIndex(6, lines[0])).toBe(114);
+            expect(getNumberAtIndex(7, lines[0])).toBe(114);
+
+            expect(getNumberAtIndex(2, lines[2])).toBe(35);
+            expect(getNumberAtIndex(3, lines[2])).toBe(35);
+
+            expect(getNumberAtIndex(6, lines[2])).toBe(633);
+            expect(getNumberAtIndex(7, lines[2])).toBe(633);
+            expect(getNumberAtIndex(8, lines[2])).toBe(633);
+
+            expect(getNumberAtIndex(0, lines[4])).toBe(617);
+            expect(getNumberAtIndex(1, lines[4])).toBe(617);
+            expect(getNumberAtIndex(2, lines[4])).toBe(617);
+
+            expect(getNumberAtIndex(7, lines[5])).toBe(58);
+            expect(getNumberAtIndex(8, lines[5])).toBe(58);
+
+            expect(getNumberAtIndex(2, lines[6])).toBe(592);
+            expect(getNumberAtIndex(3, lines[6])).toBe(592);
+            expect(getNumberAtIndex(4, lines[6])).toBe(592);
+
+            expect(getNumberAtIndex(6, lines[7])).toBe(755);
+            expect(getNumberAtIndex(7, lines[7])).toBe(755);
+            expect(getNumberAtIndex(8, lines[7])).toBe(755);
+
+            expect(getNumberAtIndex(1, lines[9])).toBe(664);
+            expect(getNumberAtIndex(2, lines[9])).toBe(664);
+            expect(getNumberAtIndex(3, lines[9])).toBe(664);
+
+            expect(getNumberAtIndex(5, lines[9])).toBe(598);
+            expect(getNumberAtIndex(6, lines[9])).toBe(598);
+            expect(getNumberAtIndex(7, lines[9])).toBe(598);
+        });
     });
 });
--- 2023/day3/index.ts
+++ 2023/day3/index.ts
@@ -1,3 +1,19 @@
+export function day3(input: string) {
+    return {
+        day: 3,
+        p1: part1(input),
+        p2: part2(input),
+    };
+}
+
+type NeighborLine = string | undefined;
+
+const DIGIT = /\d/;
+const DIGITS = /\d+/;
+const SYMBOL = /[^.\d]/;
+const GEAR = /\*/;
+const NIL = -1;
+
 /**
 Given any line L, the program needs context of the previous and next lines adjacent to L
 
@@ -9,17 +25,6 @@ Processing a line:
         2. For both `L-1` and `L+1`, check through `start-1..end+1`
     4. Repeat from step 1 until we don't find a new number
 */
-export function day3(input: string) {
-    return {
-        day: 3,
-        p1: part1(input),
-        p2: part2(input),
-    };
-}
-
-const DIGIT = /\d+/;
-const SYMBOL = /[^.\d]/;
-
 function part1(input: string): number {
     let result = 0;
 
@@ -30,24 +35,20 @@ function part1(input: string): number {
         const curr = lines[i];
         const next = lines.at(i + 1);
 
-        result += processLine(prev, curr, next);
+        result += processLinePart1(prev, curr, next);
     }
 
     return result;
 }
 
-function part2(input: string): number {
-    return -1;
-}
-
-function processLine(
-    prev: string | undefined,
+function processLinePart1(
+    prev: NeighborLine,
     curr: string,
-    next: string | undefined,
+    next: NeighborLine,
 ): number {
-    let sum = 0;
+    const getNextNumber = () => curr.match(DIGITS)?.toString();
 
-    const getNextNumber = () => curr.match(DIGIT)?.toString();
+    let sum = 0;
     let number = getNextNumber();
 
     while (number != undefined) {
@@ -75,14 +76,93 @@ function processLine(
     return sum;
 }
 
-function checkAdjacentLine(
-    line: string | undefined,
-    start: number,
-    end: number,
-) {
+function checkAdjacentLine(line: NeighborLine, start: number, end: number) {
     if (start > 0) start = start - 1;
     return line
         ?.substring(start, end + 2)
         .split("")
         .some((char) => char.match(SYMBOL));
+}
+
+/*
+Given any line L, the program needs context of the previous and next lines adjacent to L
+
+Processing a line:
+    1. We match the first `gear` of the line through RegExp
+    2. For each of it's 8 neighbors, we check for numbers using getNumberAtIndex
+    - Each number is added to a `set`
+    - If the set has exactly 2 members, the gear is valid
+        - Add the product of the numbers to result
+    3. Repeat from step 1 until we don't find more gears
+ */
+function part2(input: string): number {
+    let result = 0;
+
+    const lines = input.split("\n").filter((l) => !!l.length);
+
+    for (let i = 0; i < lines.length; i++) {
+        const prev = lines.at(i - 1);
+        const curr = lines[i];
+        const next = lines.at(i + 1);
+
+        result += processLinePart2(prev, curr, next);
+    }
+
+    return result;
+}
+
+function processLinePart2(
+    prev: NeighborLine,
+    curr: string,
+    next: NeighborLine,
+): number {
+    const getNextGear = () => curr.match(GEAR)?.toString();
+
+    let sum = 0;
+    let gear = getNextGear();
+
+    while (gear != undefined) {
+        const i = curr.indexOf(gear);
+        const neighbors = new Set<number>();
+
+        // Check all eight neighbors
+        neighbors.add(getNumberAtIndex(i - 1, prev));
+        neighbors.add(getNumberAtIndex(i, prev));
+        neighbors.add(getNumberAtIndex(i + 1, prev));
+        neighbors.add(getNumberAtIndex(i - 1, curr));
+        neighbors.add(getNumberAtIndex(i + 1, curr));
+        neighbors.add(getNumberAtIndex(i - 1, next));
+        neighbors.add(getNumberAtIndex(i, next));
+        neighbors.add(getNumberAtIndex(i + 1, next));
+
+        neighbors.delete(NIL);
+
+        if (neighbors.size == 2) {
+            sum += [...neighbors].reduce((product, n) => product * n, 1);
+        }
+
+        prev = prev?.substring(i + 1);
+        curr = curr.substring(i + 1);
+        next = next?.substring(i + 1);
+        gear = getNextGear();
+    }
+
+    return sum;
+}
+
+export function getNumberAtIndex(index: number, line: NeighborLine): number {
+    let result = line?.at(index)?.match(DIGIT)?.toString();
+    if (!line || result == undefined) {
+        return NIL;
+    }
+
+    for (let i = index + 1; line.at(i)?.match(DIGIT) != undefined; i++) {
+        result += line[i];
+    }
+
+    for (let i = index - 1; line.at(i)?.match(DIGIT) != undefined; i--) {
+        result = line[i] + result;
+    }
+
+    return +result;
 }
